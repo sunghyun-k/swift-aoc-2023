2cc287d61fd29056daf829d61511c1a97a8bc543
Sunghyun Kim <sunghyun@slingcorp.kr>
2023-12-03 20:28:50 +0900

    Day02 part1 작성

---
+++ Sources/Day02.swift
@@ -0,0 +1,62 @@
+import Algorithms
+
+struct Day02: AdventDay {
+  var data: String
+
+  var games: [Game] {
+    return data.split(separator: "\n")
+      .map {
+        let game = $0.split(separator: ":")
+        let id = Int(game[0].dropFirst(5))!
+        let sets = game[1].split(separator: "; ")
+          .map {
+            let colors = $0.split(separator: ", ")
+            var set = Game.Set()
+            colors.forEach {
+              let value = $0.split(separator: " ")
+              let count = Int(value[0])!
+              switch value[1] {
+              case "red": set.red = count
+              case "green": set.green = count
+              case "blue": set.blue = count
+              default: fatalError()
+              }
+            }
+            return set
+          }
+        return Game(id: id, sets: sets)
+      }
+  }
+
+  func part1() -> Any {
+    // 12 red cubes, 13 green cubes, and 14 blue cubes
+    let validGame = games.filter {
+      for set in $0.sets {
+        if
+          set.red > 12
+          || set.green > 13
+          || set.blue > 14
+        {
+          return false
+        }
+      }
+      return true
+    }
+    return validGame.reduce(0) { $0 + $1.id }
+  }
+  
+  func part2() -> Any {
+    return 0
+  }
+}
+
+struct Game {
+  struct Set {
+    var red: Int = 0
+    var green: Int = 0
+    var blue: Int = 0
+  }
+  
+  var id: Int
+  var sets: [Set]
+}
